0759410a3049c08c2c37057c95fac27055efb18c
Hernán Díaz Orrac <hernandiazorrac@gmail.com>
2023-02-05 18:03:19 -0300

    items in history limited to 7 and updates correctly

--- js/app.js
+++ js/app.js
@@ -2,31 +2,21 @@ let qrContainer = document.getElementById('qrContainer')
 let input = document.getElementById('input')
 let submitBtn = document.getElementById('submitBtn')
 let searchHistory = document.getElementById('searchHistory')
+let clearHistoryBtn = document.getElementById('clearHistory')
 let qr = new QRCode(qrContainer) //initialize qrcode.js library
 
 let searchHistoryArray = (localStorage.searchHistoryArray) ? JSON.parse(localStorage.searchHistoryArray) : []
 
-submitBtn.addEventListener('click', () => { //
-    qr.makeCode(input.value) //returns qr based on your text/link
+// const clearHistory = () => {
+//     searchHistoryArray = []
+//     searchHistory.innerHTML = ""
 
-    searchHistoryArray.push(input.value)
-    localStorage.searchHistoryArray = JSON.stringify(searchHistoryArray)
-    searchHistory.innerHTML = ""
-    searchHistoryArray.forEach((search) => {
-        searchHistory.innerHTML = `<li>${searchHistory.innerHTML}</li>`
-        searchHistory.querySelector('li').innerText = search
-    })
-
-    input.value = ""
-})
-
-input.addEventListener('keypress', (e) => {
-    if(e.key === 'Enter'){
-        e.preventDefault()
-        submitBtn.click()
-        input.value = ""
-    }
-})
+//     if(searchHistoryArray.length === 0){
+//         clearHistoryBtn.style.display = "none"
+//     }else{
+//         clearHistoryBtn.style.display = "flex"
+//     }
+// }
 
 input.addEventListener('input', () => {
     if(input.value.length == 0){
@@ -35,3 +25,31 @@ input.addEventListener('input', () => {
         submitBtn.disabled = false
     }
 })
+
+submitBtn.addEventListener('click', () => {
+    qr.makeCode(input.value) //returns qr based on your text/link
+    historyText()
+})
+
+input.addEventListener('keypress', (e) => {
+    if(e.key === 'Enter'){
+        e.preventDefault()
+        submitBtn.click()
+    }
+})
+
+
+
+const historyText = () => {
+    if(input.value.length != 0){ //prevents adding an empty string to history array when loading DOM
+        searchHistoryArray.push(input.value)
+    }
+
+    localStorage.searchHistoryArray = JSON.stringify(searchHistoryArray)
+    searchHistory.innerHTML = ""
+    searchHistoryArray.slice(-7).forEach((search) => {
+        searchHistory.innerHTML = `<p>QR code history</p><li>${searchHistory.innerHTML}</li>`
+        searchHistory.querySelector('li').innerText = search
+    })
+}
+
